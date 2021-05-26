16cea2f9b62e0f72a6bd5017c6bb3c89b5690cdd
Paulo Martin <paumar559@gmail.com>
2021-05-25 21:01:07 -0700

    Push to the Day 2

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -2,76 +2,73 @@ from game_of_greed.game_logic import GameLogic
 from game_of_greed.banker import Banker
 from random import randint
 
-
 class Game:
+    def __init__(self):
+        pass
+
     def default_roller():
-        return (randint(1, 6), randint(1, 6))
+        return (randint(1,6),randint(1,6))
 
-    # def play_dice(roller=default_roller):
+    def play_dice(roller=default_roller):
+        while True:
+            print("(r)oll again, (b)ank your points or (q)uit:")
+            choice = input("> ")
+            if choice == "q":
+                print("OK, bye")
+                break
+            else:
+                roll = roller()
+                roll_str = ""
+                for num in roll:
+                    roll_str += str(num) + " "
+                print(f"*** {roll_str}***")
 
-    #     while True:
-    #         print("(r)oll again, (b)ank your points or (q)uit:")
-    #         choice = input("> ")
-    #         if choice == "q":
-    #             print("OK, bye")
-    #             break
-    #         else:
-    #             roll = roller()
-    #             roll_str = ""
-    #             for num in roll:
-    #                 roll_str += str(num) + " "
-    #             roll_str_var = f"*** {roll_str} ***"
-    #             print(roll_str_var)
-    #             return roll_str_var
 
-    def start_game(self):
-        print("Starting round 1")
-        print("Rolling 6 dice...")
-        round_roll = GameLogic.roll_dice(6)
-        # dice_roll = self.roller(dice_count)
-        # (a, b, c, d, e, f) = dice_roll
-        # dice_roll_string = "*** "
-        # for die in dice_roll:
-        #     dice_roll_string += f"{str(die)} "
-        # dice_roll_string += "***"
-        # print(dice_roll_string)
-        print("*** 3 2 5 4 3 3 ***")
-        print("Enter dice to keep, or (q)uit:")
-
-    def play(self, roller=None):
-        self._roller = roller or GameLogic.roll_dice
-
+    def play(self, roller = default_roller):
         print("Welcome to Game of Greed")
         print("(y)es to play or (n)o to decline")
-        ans = input("> ")
-
-        if ans == "n":
+        start_game = input("> ")
+        if start_game == "n":
             print("OK. Maybe another time")
-
-        if ans == "y":
-            self.start_game()
-
+            
+        if start_game == "y" :
+            print("Starting round 1")
+            print("Rolling 6 dice...")
+            print("*** 4 2 6 4 6 5 ***")
+            print("Enter dice to keep, or (q)uit:")
+            
         ans2 = input("> ")
-        if ans2 == "5":
-            score = GameLogic.calculate_score(ans2)
-            print(f"You have {score} unbanked points and 5 dice remaining")
 
+        if type(int(ans2)) is int:
+            score = self.score_round(ans2)
+            print(f"You have {score[1]} unbanked points and {score[0]} dice remaining")
+            
         if ans2 == "q":
             print("Thanks for playing. You earned 0 points")
             return
 
         print("(r)oll again, (b)ank your points or (q)uit:")
 
-        ans3 = input("> ")
-        if ans3 == "b":
-            print(f"You banked 50 points in round 1")
-            print("Total score is 50 points")
+        # ans3 = input("> ")
+        # if ans3 == "b":
+        #     bank = Banker.bank()
+        #     print(f"You banked {bank} points in round 1")
+        #     print("Total score is 50 points")
 
         print("Starting round 2")
-        print("Rolling 6 dice...")
-        print("*** 5 2 3 2 1 4 ***")
-        print("Enter dice to keep, or (q)uit:")
 
-        ans4 = input("> ")
-        if ans4 == "q":
-            print("Thanks for playing. You earned 50 points")
+    def score_round(self, string):
+        dice = []
+        for num in string:
+            dice.append(num)
+        
+        result = []
+        result.append(6 - len(dice))
+        dice = tuple(dice)
+        result.append(GameLogic.calculate_score(dice))
+
+
+        return result
+
+        
+
